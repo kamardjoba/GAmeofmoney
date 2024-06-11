9ffe0051a9ecdfcb9bf8817b2b395eaa0bcebc6b
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-06-11 23:59:50 +0200

    initial

--- src/ref.js
+++ src/ref.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './ref.css';
 
 const Ref = ({ onClose, userId, telegramLink }) => {
-    const [referralLink, setReferralLink] = useState(telegramLink);
+    const [referralLink] = useState(telegramLink);
     const [referralCount, setReferralCount] = useState(0);
 
     useEffect(() => {
