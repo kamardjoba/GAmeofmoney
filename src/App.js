742f69bbe8d0ff60a09e6c38d354cc477ed53bff
Aferist163 <aferist.game.163@gmail.com>
2024-06-11 16:37:23 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -328,11 +328,6 @@ function App() {
             <MiniGame onClose={handleCloseMiniGame} />
         )}
 
-        <div className="referral-section">
-          <p>Ваш реферальный код: {referralCode}</p>
-          <p>Поделитесь этой ссылкой, чтобы пригласить друзей:</p>
-          <p>{telegramLink}</p>
-        </div>
       </div>
   );
 }
