638ae1626d3dd401ea2f128765a06620b1a65308
Aferist163 <aferist.game.163@gmail.com>
2024-07-03 01:01:14 +0200

    initial

--- src/Js/earn.js
+++ src/Js/earn.js
@@ -43,7 +43,10 @@ const Earn = ({ onClose, isVisibleClaim, setVisibleClaim, isVisibleComplated, se
             if (data.isSubscribed) {
                 setVisibleClaim(true);
                 localStorage.setItem('VisibleChanel', 'false')
-            }        
+            } else {
+                localStorage.setItem('VisibleChanel', 'true'); // Store as a string
+              }
+
         };
         checkSubscriptionOnMount();
     }, [onCheckSubscription, userId, isVisibleChanel]);
