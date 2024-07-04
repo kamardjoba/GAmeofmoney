2b57e40fa8156104bec339e1e98977736e1a9cd2
Aferist163 <aferist.game.163@gmail.com>
2024-07-04 14:12:28 +0200

    initial

--- src/Js/loot.js
+++ src/Js/loot.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import '../Css/loot.css';
 import Craft from './craft'
+import znakLogo from '../IMG/Znak.png';
+import Loot_Znak from '../IMG/Loot_ZNAK.png';
 
 const Loot = ({onClose,handleCheckboxChange}) => {
 
@@ -13,9 +15,32 @@ const Loot = ({onClose,handleCheckboxChange}) => {
         setisCraftOpen(event.target.checked);
       };
 
-    return (
+    return (  
         <div className={`Ref_Earn_Shop_Window ${isClosingLootForAnim ? 'closing' : ''}`} id='LootWindow'>
-           <input id="checkbox_toggle" type="checkbox" class="check" onChange={(event) => {handleCheckboxChangeDiv(event); handleCheckboxChange(event); }}/>
+
+            <div className="Ref_Earn_BoxBorder">
+                <div className='Ref_Earn_Box'>
+                    <img src={Loot_Znak} alt='Loot_Znak' height={"80%"}/>
+                </div>
+                <div className='Ref_Earn_BoxTitle'>
+                    <div className='Ref_Earn_BoxUp'>
+                        <p>WEEKLY TASKS</p>
+                    </div>
+                    <div className='Ref_Earn_BoxDown'>
+                        <div className='Ref_Earn_BoxLeft'>
+                            <img src={znakLogo} alt='znakLogo' height={"50%"}/>
+                        </div>
+                        <div className='Ref_Earn_BoxRight'>
+                            <p>COMPLATE WEEKLY</p>
+                            <p>TASKS AND <span className="Ref_Earn_Purple">EARN</span></p>
+                            <p className="Ref_Earn_Purple" >MORE ITEMS</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='SWITCHBTN'>
+            <input id="checkbox_toggle" type="checkbox" class="check" onChange={(event) => {handleCheckboxChangeDiv(event); handleCheckboxChange(event); }}/>
                 <div class="checkbox">
                     <label class="slide" for="checkbox_toggle">
                     <label class="toggle" for="checkbox_toggle"></label>
@@ -23,8 +48,29 @@ const Loot = ({onClose,handleCheckboxChange}) => {
                     <label class="text" for="checkbox_toggle" id="CraftTxt">CRAFT</label>
                     </label>
                 </div>
+            </div>
+            <div className='Loot_Wnd'>
+                <div className="Loot_Scroll_Menu">
 
-                <p>Loot</p>
+                    <div className="Loot_Card">
+                        <p>1</p>
+                    </div>
+                    <div className="Loot_Card">
+                        <p>2</p>
+                    </div>
+                    <div className="Loot_Card">
+                        <p>3</p>
+                    </div>
+                    <div className="Loot_Card">
+                        <p>4</p>
+                    </div>
+                    <div className="Loot_Card">
+                        <p>5</p>
+                    </div>
+
+                </div>    
+            </div>
+                
         <button id='CloseDebug' onClick={(event) => {onClose(event); handleCloseLootAnim(event); }}>X</button>
 
         {isCraftOpen && (
