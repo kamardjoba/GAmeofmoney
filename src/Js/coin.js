00df93db044c3125a316e96e900c08884de3e18f
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-07-05 02:30:37 +0200

    initial

--- src/Js/coin.js
+++ src/Js/coin.js
@@ -4,72 +4,65 @@ import coinImage from '../IMG/88nog.png';
 import ink from '../IMG/ink.png';
 import '../Css/coin.css';
 
-const Coindiv = ({ onClick, coinPerClick, energyNow}) => {
+const Coindiv = ({ onClick, coinPerClick, energyNow }) => {
   const [clicksArray, setClicksArray] = useState([]);
 
-  const handleInteractionStart = (event) => {
-    const touchEvent = event.type === 'touchstart' ? event.touches[0] : event;
-    const rect = event.target.getBoundingClientRect();
-    const x = touchEvent.clientX - rect.left;
-    const y = touchEvent.clientY - rect.top;
-    const rotateX = ((y / rect.height) - 0.5) * -40;
-    const rotateY = ((x / rect.width) - 0.5) * 40;
-    
-    event.target.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-  };
-    
-  const handleInteractionEnd = (event) => {
-    event.target.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  const handleTouchStart = (event) => {
+    event.preventDefault(); // Остановка стандартного поведения, если необходимо
+    handleTouch(event);
   };
 
-  const NumberUpAnim = (event) => {
-    const touchEvent = event.type === 'touchstart' ? event.touches[0] : event;
-    if (coinPerClick >  energyNow) return;
-    
-    const x = touchEvent.clientX;
-    const y = touchEvent.clientY;
-    
-    setClicksArray((prevClicks) => [
-    ...prevClicks,
-    { id: Date.now(), x, y, value: coinPerClick },
-    ]);
+  const handleTouchMove = (event) => {
+    event.preventDefault(); // Остановка стандартного поведения, если необходимо
+    handleTouch(event);
+  };
 
-    onClick();
+  const handleTouch = (event) => {
+    const touches = event.touches;
+    for (let i = 0; i < touches.length; i++) {
+      const touch = touches[i];
+      const rect = event.target.getBoundingClientRect();
+      const x = touch.clientX - rect.left;
+      const y = touch.clientY - rect.top;
 
-    if (navigator.vibrate) {
-      navigator.vibrate(10);
+      setClicksArray((prevClicks) => [
+        ...prevClicks,
+        { id: Date.now(), x: touch.clientX, y: touch.clientY, value: coinPerClick },
+      ]);
+
+      onClick();
+
+      if (navigator.vibrate) {
+        navigator.vibrate(10);
+      }
     }
-
   };
 
   return (
     <motion.div className='Podsos'>
-      
-      <img  src={coinImage} 
-            alt="Coin" 
-            height="90%" 
-            //onMouseDown={handleInteractionStart}
-            //onMouseUp={handleInteractionEnd}
-            //onClick={onClick}
-            onTouchEnd={handleInteractionEnd}
-            onTouchStart={(event) => {handleInteractionStart(event); NumberUpAnim(event); }}
+      <img
+        src={coinImage}
+        alt="Coin"
+        height="90%"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={(event) => event.target.style.transform = 'rotateX(0deg) rotateY(0deg)'}
       />
       
       <AnimatePresence>
         {clicksArray.map((click) => (
-            <motion.div
-              key={click.id}
-              className="click-value"
-              initial={{ opacity: 1, y: 0 }}
-              animate={{ opacity: 0, y: -150 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 2, ease: "ease-out" }}   
-              style={{ top: click.y, left: click.x}}
-            >
-              <p>+{click.value}</p>
-              <img id="inktap" src={ink} alt='ink' width={"100%"}/>
-              
-            </motion.div>
+          <motion.div
+            key={click.id}
+            className="click-value"
+            initial={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 0, y: -150 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 2, ease: "ease-out" }}
+            style={{ top: click.y, left: click.x }}
+          >
+            <p>+{click.value}</p>
+            <img id="inktap" src={ink} alt='ink' width={"100%"}/>
+          </motion.div>
         ))}
       </AnimatePresence>
     </motion.div>
