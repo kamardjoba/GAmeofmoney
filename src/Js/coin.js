e9a11546fdc5aa522e980588a5b5b541fdcfbe63
Aferist163 <aferist.game.163@gmail.com>
2024-07-08 05:22:03 +0200

    initial

--- src/Js/coin.js
+++ src/Js/coin.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import '../Css/coin.css';
 
-const Coindiv = ({coinImage, onClick, coinPerClick, energyNow, ink}) => {
+const Coindiv = ({ coinImage, onClick, coinPerClick, energyNow, ink }) => {
   const [clicksArray, setClicksArray] = useState([]);
 
   const handleInteractionStart = (event) => {
@@ -12,24 +12,25 @@ const Coindiv = ({coinImage, onClick, coinPerClick, energyNow, ink}) => {
     const y = touchEvent.clientY - rect.top;
     const rotateX = ((y / rect.height) - 0.5) * -40;
     const rotateY = ((x / rect.width) - 0.5) * 40;
-    
+
     event.target.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
   };
-    
+
   const handleInteractionEnd = (event) => {
     event.target.style.transform = 'rotateX(0deg) rotateY(0deg)';
   };
 
   const NumberUpAnim = (event) => {
     const touchEvent = event.type === 'touchstart' ? event.touches[0] : event;
-    if (coinPerClick >  energyNow) return;
-    
-    const x = touchEvent.clientX;
-    const y = touchEvent.clientY;
-    
+    if (coinPerClick > energyNow) return;
+
+    const rect = event.target.getBoundingClientRect();
+    const x = touchEvent.clientX - rect.left;
+    const y = touchEvent.clientY - rect.top;
+
     setClicksArray((prevClicks) => [
-    ...prevClicks,
-    { id: Date.now(), x, y, value: coinPerClick },
+      ...prevClicks,
+      { id: Date.now(), x, y, value: coinPerClick },
     ]);
 
     onClick();
@@ -37,36 +38,41 @@ const Coindiv = ({coinImage, onClick, coinPerClick, energyNow, ink}) => {
     if (navigator.vibrate) {
       navigator.vibrate(10);
     }
-
   };
 
   return (
-    <motion.div className='Podsos'>
-      
-      <img  src={coinImage} 
-            alt="Coin" 
-            //onMouseDown={handleInteractionStart}
-            //onMouseUp={handleInteractionEnd}
-            //onClick={onClick}
-            onTouchEnd={handleInteractionEnd}
-            onTouchStart={(event) => {handleInteractionStart(event); NumberUpAnim(event); }}
+    <motion.div
+      className='Podsos'
+      onTouchStart={(event) => {
+        handleInteractionStart(event);
+        NumberUpAnim(event);
+      }}
+      onTouchEnd={handleInteractionEnd}
+    >
+      <img
+        src={coinImage}
+        alt="Coin"
+        onTouchEnd={handleInteractionEnd}
+        onTouchStart={(event) => {
+          handleInteractionStart(event);
+          NumberUpAnim(event);
+        }}
       />
-      
+
       <AnimatePresence>
         {clicksArray.map((click) => (
-            <motion.div
-              key={click.id}
-              className="click-value"
-              initial={{ opacity: 1, y: 0 }}
-              animate={{ opacity: 0, y: -150 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 2, ease: "ease-out" }}   
-              style={{ top: click.y, left: click.x}}
-            >
-              <p>+{click.value}</p>
-              <img id="inktap" src={ink} alt='ink' width={"100%"}/>
-              
-            </motion.div>
+          <motion.div
+            key={click.id}
+            className="click-value"
+            initial={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 0, y: -150 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 2, ease: "ease-out" }}
+            style={{ top: click.y, left: click.x }}
+          >
+            <p>+{click.value}</p>
+            <img id="inktap" src={ink} alt='ink' width={"100%"} />
+          </motion.div>
         ))}
       </AnimatePresence>
     </motion.div>
